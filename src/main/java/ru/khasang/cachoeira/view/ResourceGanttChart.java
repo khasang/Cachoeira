07d01705184e172d6a81fcfe4e73017cc2145f83
truesik <truesik@gmail.com>
2015-12-09 01:57:14 +0300

    Диаграмма ганта для вкладки Ресурсы

---
+++ src/main/java/ru/khasang/cachoeira/view/ResourceGanttChart.java
@@ -0,0 +1,63 @@
+package ru.khasang.cachoeira.view;
+
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import ru.khasang.cachoeira.controller.IController;
+import ru.khasang.cachoeira.view.resourcepaneganttchart.ResourcePaneDateLine;
+import ru.khasang.cachoeira.view.resourcepaneganttchart.ResourcePaneGridLayer;
+import ru.khasang.cachoeira.view.resourcepaneganttchart.ResourcePaneObjectsLayer;
+
+/**
+ * Created by truesik on 08.12.2015.
+ */
+public class ResourceGanttChart extends VBox {
+    private ResourcePaneGridLayer resourcePaneGridLayer;        //слой с сеткой
+    private ResourcePaneObjectsLayer resourcePaneObjectsLayer;  //слой с объектами диаграммы (задачи, группы, ...)
+    private ResourcePaneDateLine resourcePaneDateLine;          //шкала с датами
+    private int columnWidth;            //ширина колонок
+    private UIControl uiControl;
+
+    public ResourceGanttChart(IController controller, UIControl uiControl, int columnWidth) {
+        this.columnWidth = columnWidth;
+        this.uiControl = uiControl;
+
+        resourcePaneGridLayer = new ResourcePaneGridLayer(columnWidth);
+        resourcePaneObjectsLayer = new ResourcePaneObjectsLayer(controller, columnWidth);
+        resourcePaneObjectsLayer.setUIControl(uiControl);
+        ScrollPane verticalScrollPane = new ScrollPane(resourcePaneObjectsLayer);
+        verticalScrollPane.setFitToWidth(true);
+        verticalScrollPane.getStylesheets().add(this.getClass().getResource("/css/scrollpane.css").toExternalForm()); //делаем вертикальный скроллпэйн прозрачным
+
+        StackPane diagramPane = new StackPane(resourcePaneGridLayer, verticalScrollPane); //нижний слой сетка, а над ним располагается слой с задачами
+        VBox.setVgrow(diagramPane, Priority.ALWAYS);
+
+        resourcePaneDateLine = new ResourcePaneDateLine(controller, columnWidth);
+        resourcePaneDateLine.setUiControl(uiControl);
+        resourcePaneDateLine.initDateLine();
+        VBox vBox = new VBox(resourcePaneDateLine, diagramPane);
+        VBox.setVgrow(resourcePaneDateLine, Priority.NEVER);
+
+        ScrollPane horizontalScrollPane = new ScrollPane(vBox);
+        horizontalScrollPane.setFitToHeight(true);
+        horizontalScrollPane.getStyleClass().add("edge-to-edge"); //убирает подсветку синюю границу вокруг скроллпэйна
+        horizontalScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        horizontalScrollPane.setPannable(true); //дает возможность двигать диаграмму зажав кнопку мышки, но работает только если жать на шкалу времени
+
+        this.getChildren().addAll(horizontalScrollPane);
+        VBox.setVgrow(horizontalScrollPane, Priority.ALWAYS);
+    }
+
+    public ResourcePaneGridLayer getResourcePaneGridLayer() {
+        return resourcePaneGridLayer;
+    }
+
+    public ResourcePaneObjectsLayer getResourcePaneObjectsLayer() {
+        return resourcePaneObjectsLayer;
+    }
+
+    public ResourcePaneDateLine getResourcePaneDateLine() {
+        return resourcePaneDateLine;
+    }
+}
---
+++ src/main/java/ru/khasang/cachoeira/view/resourcepaneganttchart/ResourcePaneTaskBar.java
@@ -0,0 +1,189 @@
+package ru.khasang.cachoeira.view.resourcepaneganttchart;
+
+import javafx.beans.binding.Bindings;
+import javafx.collections.ListChangeListener;
+import javafx.scene.Cursor;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tooltip;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import ru.khasang.cachoeira.controller.IController;
+import ru.khasang.cachoeira.model.IResource;
+import ru.khasang.cachoeira.model.ITask;
+import ru.khasang.cachoeira.view.UIControl;
+
+import java.time.temporal.ChronoUnit;
+
+/**
+ * Created by truesik on 04.11.2015.
+ */
+public class ResourcePaneTaskBar extends Rectangle {
+    private static final double TASK_HEIGHT = 18;   //высота прямоугольника задачи
+    private static final double rowHeight = 24;
+
+    private IController controller;
+    private ITask task;
+    private int rowIndex;                        //координата Y (строка задачи)
+    private int columnWidth;
+    private UIControl uiControl;
+    private IResource resource;
+    private boolean wasMoved;
+    private ContextMenu contextMenu;
+
+    public ResourcePaneTaskBar(IController controller, ITask task, int columnWidth, UIControl uiControl, IResource resource) {
+        this.controller = controller;
+        this.task = task;
+        this.columnWidth = columnWidth;
+        this.uiControl = uiControl;
+        this.resource = resource;
+
+        rowIndex = controller.getProject().getResourceList().indexOf(resource);
+
+        this.setFill(Color.valueOf("#03A9F4"));    //цвет прямоугольника
+        this.setStroke(Color.valueOf("#B3E5FC"));  //цвет окантовки
+        this.setArcHeight(5);                      //сгругление углов
+        this.setArcWidth(5);                       //сгругление углов
+
+        calculateStartPoint();
+        this.setLayoutY((rowIndex * rowHeight) + 3);
+        calculateWidth();
+        this.setHeight(TASK_HEIGHT);
+
+        /**
+         * Следим за изменениями в списке ресурсов, если произошло добавление или удаление элемента в списке,
+         * то пересчитываем индексы у элементов на диаграмме
+         */
+        controller.getProject().getResourceList().addListener(new ListChangeListener<IResource>() {
+            @Override
+            public void onChanged(Change<? extends IResource> change) {
+                while (change.next()) {
+                    if (change.wasRemoved() || change.wasAdded()) {
+                        rowIndex = controller.getProject().getResourceList().indexOf(resource);
+                        setLayoutY((rowIndex * rowHeight) + 3);
+                    }
+                }
+            }
+        });
+        /** Следим за изменением начальной и конечной даты */
+        task.startDateProperty().addListener((observable, oldValue, newValue) -> {
+            if (!wasMoved) {
+                calculateStartPoint();
+                calculateWidth();
+            }
+        });
+        task.finishDateProperty().addListener((observable, oldValue, newValue) -> {
+            if (!wasMoved) {
+                calculateWidth();
+            }
+        });
+
+        //подсветка при наведении
+        this.hoverProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                this.setFill(Color.valueOf("#41C2FA"));
+                this.setStroke(Color.valueOf("#72D1FA"));
+            } else {
+                this.setFill(Color.valueOf("#03A9F4"));    //цвет прямоугольника
+                this.setStroke(Color.valueOf("#B3E5FC"));
+            }
+        });
+
+        setContextMenu(); //контекстное меню при нажатии на прямоугольник, добавлено для теста (код нужно дописать)
+        setTooltip();
+        enableDrag();
+    }
+
+    private void calculateStartPoint() {
+        double startDay = (ChronoUnit.DAYS.between(controller.getProject().getStartDate(), task.getStartDate())) * columnWidth;
+        this.setX(startDay - 2); //"2" - подгонка под сетку
+    }
+
+    private void calculateWidth() {
+        double taskWidth = (ChronoUnit.DAYS.between(task.getStartDate(), task.getFinishDate()) * columnWidth);
+        this.setWidth(taskWidth);
+    }
+
+    public void setContextMenu() {
+        Menu setResource = new Menu("Назначить ресурс");
+        MenuItem getProperties = new MenuItem("Свойства");
+        getProperties.setOnAction(event -> controller.selectedTaskProperty().setValue(this.task));
+        MenuItem removeTask = new MenuItem("Удалить задачу");
+
+        contextMenu = new ContextMenu(setResource, getProperties, removeTask);
+    }
+
+    private void setTooltip() {
+        Tooltip tooltip = new Tooltip();
+        tooltip.textProperty().bind(Bindings
+                .concat(Bindings
+                        .when(task.descriptionProperty().isNull().or(task.descriptionProperty().isEmpty()))
+                        .then("")
+                        .otherwise(Bindings.concat("Описание: ").concat(task.descriptionProperty()).concat("\n")))
+                .concat("Дата начала: ").concat(task.startDateProperty()).concat("\n")
+                .concat("Дата окончания: ").concat(task.finishDateProperty()).concat("\n")
+                .concat("Продолжительность: ").concat(ChronoUnit.DAYS.between(task.startDateProperty().getValue(), task.finishDateProperty().getValue())).concat("\n")
+                .concat("Прогресс: ").concat(task.donePercentProperty()).concat("\n")
+                .concat("Стоимость: ").concat(task.costProperty()).concat("\n")
+                .concat("Приоритет: ").concat(task.priorityTypeProperty()));
+        Tooltip.install(this, tooltip);
+    }
+
+    public ITask getTask() {
+        return task;
+    }
+
+    public IResource getResource() {
+        return resource;
+    }
+
+    /**
+     * Drag'n'Drop
+     */
+    private void enableDrag() {
+        final Delta dragDelta = new Delta();
+        setOnMousePressed(event -> {
+            /** Выделяем нужный элемент в таблице */
+            int i = controller.getProject().getTaskList().indexOf(task);
+            uiControl.getMainWindow().getDiagramPaneController().getTaskPaneController().getTaskTreeTableView().getSelectionModel().select(i);
+            if (event.isPrimaryButtonDown()) {
+                // record a delta distance for the drag and drop operation.
+                dragDelta.x = getX() - event.getX();
+                getScene().setCursor(Cursor.MOVE);
+            } else {
+                getScene().setCursor(Cursor.DEFAULT);
+            }
+            /** Условие для контекстного меню */
+            if (event.isSecondaryButtonDown()) {
+                contextMenu.show(ResourcePaneTaskBar.this, event.getScreenX(), event.getScreenY());
+            }
+        });
+        setOnMouseDragged(event -> {
+            if (event.isPrimaryButtonDown()) {
+                double newX = event.getX() + dragDelta.x;
+                if (newX > 0 && newX < getScene().getWidth()) {
+                    /** Хреначим привязку к сетке */
+                    double v = newX / columnWidth; // Делим координату на ширину столбца, получаем цифру в днях с десятыми
+                    long round = Math.round(v); // Убираем десятые, чтобы был ровно день
+                    wasMoved = true; // Когда начитаем двигать, то тру, чтобы не началась рекурсия
+                    task.setStartDate(controller.getProject().getStartDate().plusDays(round));
+                    task.setFinishDate(task.getStartDate().plusDays(Math.round(getWidth() / columnWidth)));
+                    long l = round * columnWidth; // Конвертим обратно в пиксели
+                    setX(l - 2);
+                    wasMoved = false; // Когда окончили движение фолз
+                }
+            }
+        });
+        setOnMouseExited(event -> {
+            if (!event.isPrimaryButtonDown()) {
+                getScene().setCursor(Cursor.DEFAULT);
+            }
+        });
+    }
+
+    // records relative x co-ordinate.
+    private class Delta {
+        double x;
+    }
+}
