5645a3c1a98cce83a9bff8db7dd51afab53f5c15
truesik <truesik@gmail.com>
2015-12-10 21:41:00 +0300

    hotfix

--- src/main/java/ru/khasang/cachoeira/view/TaskPaneController.java
+++ src/main/java/ru/khasang/cachoeira/view/TaskPaneController.java
@@ -154,6 +154,7 @@ public class TaskPaneController {
                 }
                 if (c.wasAdded()) {
                     System.out.println("Main Window Task Added!");
+                    uiControl.getMainWindow().getDiagramPaneController().getResourcePaneController().getResourceGanttChart().getResourcePaneObjectsLayer().refreshResourceDiagram();
                 }
                 if (c.wasRemoved()) {
                     System.out.println("Main Window Task Removed");
