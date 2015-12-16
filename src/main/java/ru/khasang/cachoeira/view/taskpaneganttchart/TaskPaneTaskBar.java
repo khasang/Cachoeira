92595ead437f8267f160f9c53c0c646ccd142e24
truesik <truesik@gmail.com>
2015-12-16 23:31:25 +0300

    Добавил возможность изменения метки задачи с левой стороны

--- src/main/java/ru/khasang/cachoeira/view/taskpaneganttchart/TaskPaneTaskBar.java
+++ src/main/java/ru/khasang/cachoeira/view/taskpaneganttchart/TaskPaneTaskBar.java
@@ -1,12 +1,16 @@
 package ru.khasang.cachoeira.view.taskpaneganttchart;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.collections.ListChangeListener;
-import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 import javafx.scene.control.*;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 import ru.khasang.cachoeira.model.IResource;
 import ru.khasang.cachoeira.model.ITask;
 import ru.khasang.cachoeira.view.UIControl;
@@ -18,7 +22,7 @@ import java.time.temporal.ChronoUnit;
 /**
  * Created by truesik on 04.11.2015.
  */
-public class TaskPaneTaskBar extends StackPane {
+public class TaskPaneTaskBar extends Pane {
     private static final double TASK_HEIGHT = 18;   //высота прямоугольника задачи
     private static final double rowHeight = 31;
 
@@ -27,15 +31,17 @@ public class TaskPaneTaskBar extends StackPane {
     private int columnWidth;
     private boolean wasMoved;
     private ContextMenu contextMenu;
+    private Rectangle backgroundRectangle;
 
     public TaskPaneTaskBar(int columnWidth) {
         this.columnWidth = columnWidth;
-        setAlignment(Pos.CENTER_LEFT);
+//        setAlignment(Pos.CENTER_LEFT);
+
     }
 
     public void createTaskRectangle(UIControl uiControl,
                                     ITask task) {
-        Rectangle backgroundRectangle = new Rectangle();
+        backgroundRectangle = new Rectangle();
         backgroundRectangle.setFill(Color.valueOf("#03A9F4"));    //цвет прямоугольника
         backgroundRectangle.setStroke(Color.valueOf("#B3E5FC"));  //цвет окантовки
         backgroundRectangle.setArcHeight(5);                      //сгругление углов
@@ -48,12 +54,16 @@ public class TaskPaneTaskBar extends StackPane {
         donePercentRectangle.setFill(Color.valueOf("#0381f4"));
         donePercentRectangle.arcHeightProperty().bind(backgroundRectangle.arcHeightProperty());
         donePercentRectangle.arcWidthProperty().bind(backgroundRectangle.arcWidthProperty());
+        donePercentRectangle.yProperty().bind(backgroundRectangle.yProperty().add(1.25));
         donePercentRectangle.heightProperty().bind(backgroundRectangle.heightProperty().subtract(2.5));
         donePercentRectangle.widthProperty().bind(
                 backgroundRectangle.widthProperty().divide(100).multiply(task.donePercentProperty()));
+        donePercentRectangle.onMousePressedProperty().bind(backgroundRectangle.onMousePressedProperty());
+        donePercentRectangle.onMouseDraggedProperty().bind(backgroundRectangle.onMouseDraggedProperty());
 
         this.getChildren().add(backgroundRectangle);
         this.getChildren().add(donePercentRectangle);
+        enableResize(uiControl);
 
         setListeners(uiControl, task, backgroundRectangle, donePercentRectangle);
     }
@@ -92,7 +102,11 @@ public class TaskPaneTaskBar extends StackPane {
         uiControl.getController().getProject().getTaskList().addListener((ListChangeListener<ITask>) c -> {
             while (c.next()) {
                 if (c.wasRemoved() || c.wasAdded()) {
-                    this.setLayoutY(taskY(uiControl.getController().getProject().getTaskList().indexOf(task)));
+                    KeyValue endKeyValue = new KeyValue(this.layoutYProperty(), taskY(uiControl.getController().getProject().getTaskList().indexOf(task)), Interpolator.SPLINE(0.4, 0, 0.2, 1));
+                    KeyFrame endKeyFrame = new KeyFrame(Duration.millis((rowIndex + 1) * 150), endKeyValue);
+                    Timeline timeline = new Timeline(endKeyFrame);
+                    timeline.play();
+//                    this.setLayoutY(taskY(uiControl.getController().getProject().getTaskList().indexOf(task)));
                 }
             }
         });
@@ -158,7 +172,7 @@ public class TaskPaneTaskBar extends StackPane {
                            ITask task) {
         final Delta dragDelta = new Delta();
         final OldRound oldRound = new OldRound();
-        setOnMousePressed(event -> {
+        backgroundRectangle.setOnMousePressed(event -> {
             /** Выделяем нужный элемент в таблице */
             uiControl.getMainWindow().getDiagramPaneController().getTaskPaneController().getTaskTreeTableView().getSelectionModel().select(rowIndex);
             if (event.isPrimaryButtonDown()) {
@@ -173,7 +187,7 @@ public class TaskPaneTaskBar extends StackPane {
                 contextMenu.show(TaskPaneTaskBar.this, event.getScreenX(), event.getScreenY());
             }
         });
-        setOnMouseDragged(event -> {
+        backgroundRectangle.setOnMouseDragged(event -> {
             if (event.isPrimaryButtonDown()) {
                 double newX = event.getSceneX() + dragDelta.x;
                 if (newX > 0 && newX < getScene().getWidth()) {
@@ -182,18 +196,103 @@ public class TaskPaneTaskBar extends StackPane {
                     long newRound = Math.round(v); // Убираем десятые, чтобы был ровно день
                     if (newRound != oldRound.old) {
                         oldRound.old = newRound;
-                        wasMoved = true; // Когда начитаем двигать, то тру, чтобы не началась рекурсия
-                        task.setStartDate(uiControl.getController().getProject().getStartDate().plusDays(newRound));
-                        task.setFinishDate(task.getStartDate().plusDays(Math.round(getWidth() / columnWidth)));
                         long l = newRound * columnWidth; // Конвертим обратно в пиксели
                         setLayoutX(l - 2);
+                        wasMoved = true; // Когда начитаем двигать, то тру, чтобы не началась рекурсия
+                        task.setStartDate(uiControl.getController().getProject().getStartDate().plusDays(newRound));
+                        task.setFinishDate(task.getStartDate().plusDays(Math.round(this.getWidth() / columnWidth)));
                         wasMoved = false; // Когда окончили движение фолз
                     }
                 }
             }
         });
-        setOnMouseExited(event -> {
+        backgroundRectangle.setOnMouseExited(event -> {
             if (!event.isPrimaryButtonDown()) {
+                getScene().setCursor(Cursor.DEFAULT);
+            }
+        });
+    }
+
+    public void enableResize(UIControl uiControl) {
+
+        Rectangle resizeHandleLeft = new Rectangle();
+        this.getChildren().add(resizeHandleLeft);
+        resizeHandleLeft.setFill(Color.GOLD);
+        resizeHandleLeft.setWidth(10);
+        // bind to top left corner of Rectangle:
+        resizeHandleLeft.xProperty().bind(backgroundRectangle.xProperty());
+        resizeHandleLeft.heightProperty().bind(backgroundRectangle.heightProperty());
+        resizeHandleLeft.yProperty().bind(backgroundRectangle.yProperty());
+        resizeHandleLeft.hoverProperty().addListener((observable, oldValue, newValue) -> {
+            if (resizeHandleLeft.isHover()) {
+                getScene().setCursor(Cursor.H_RESIZE);
+            } else {
+                getScene().setCursor(Cursor.DEFAULT);
+            }
+        });
+
+
+        Rectangle resizeHandleRight = new Rectangle();
+        this.getChildren().add(resizeHandleRight);
+        resizeHandleRight.setFill(Color.TRANSPARENT);
+        resizeHandleRight.setWidth(10);
+        // bind to bottom right corner of Rectangle:
+        resizeHandleRight.xProperty().bind(backgroundRectangle.widthProperty().subtract(10));
+        resizeHandleRight.heightProperty().bind(backgroundRectangle.heightProperty());
+        resizeHandleRight.yProperty().bind(backgroundRectangle.yProperty());
+        resizeHandleRight.hoverProperty().addListener((observable, oldValue, newValue) -> {
+            if (resizeHandleRight.isHover()) {
+                getScene().setCursor(Cursor.H_RESIZE);
+            } else {
+                getScene().setCursor(Cursor.DEFAULT);
+            }
+        });
+
+//        resizeHandleLeft.setOnMouseDragged(event -> {
+//            if (mouseLocation.value != null) {
+//                double deltaX = event.getSceneX() - mouseLocation.value.getX();
+//                double newX = backgroundRectangle.getX() + deltaX;
+//                if (newX >= 10
+//                        && newX <= backgroundRectangle.getX() + backgroundRectangle.getWidth() - 10) {
+//                    backgroundRectangle.setX(newX);
+//                    backgroundRectangle.setWidth(backgroundRectangle.getWidth() - deltaX);
+//                }
+//                mouseLocation.value = new Point2D(event.getSceneX(), event.getSceneY());
+//            }
+//        });
+
+        final Delta dragDelta = new Delta();
+        final OldRound oldRound = new OldRound();
+        resizeHandleRight.setOnMousePressed(event -> {
+            /** Выделяем нужный элемент в таблице */
+            uiControl.getMainWindow().getDiagramPaneController().getTaskPaneController().getTaskTreeTableView().getSelectionModel().select(rowIndex);
+            if (event.isPrimaryButtonDown()) {
+                // record a delta distance for the drag and drop operation.
+                dragDelta.x = backgroundRectangle.getWidth() - event.getSceneX();
+                getScene().setCursor(Cursor.H_RESIZE);
+            } else {
+                getScene().setCursor(Cursor.DEFAULT);
+            }
+        });
+        resizeHandleRight.setOnMouseDragged(event -> {
+            if (event.isPrimaryButtonDown()) {
+                getScene().setCursor(Cursor.H_RESIZE);
+                double newX = event.getSceneX() + dragDelta.x;
+                if (newX >= columnWidth && newX < getScene().getWidth()) {
+                    /** Хреначим привязку к сетке */
+                    double v = newX / columnWidth; // Делим координату на ширину столбца, получаем цифру в днях с десятыми
+                    long newRound = Math.round(v); // Убираем десятые, чтобы был ровно день
+                    if (newRound != oldRound.old) {
+                        oldRound.old = newRound;
+                        long l = newRound * columnWidth; // Конвертим обратно в пиксели
+                        backgroundRectangle.setWidth(l);
+                        wasMoved = true; // Когда начитаем двигать, то тру, чтобы не началась рекурсия
+                        System.out.println(backgroundRectangle.getWidth() / columnWidth);
+                        task.setFinishDate(task.getStartDate().plusDays(Math.round(backgroundRectangle.getWidth() / columnWidth)));
+                        wasMoved = false; // Когда окончили движение фолз
+                    }
+                }
+            } else {
                 getScene().setCursor(Cursor.DEFAULT);
             }
         });
@@ -214,5 +313,9 @@ public class TaskPaneTaskBar extends StackPane {
 
     private class OldRound {
         long old;
+    }
+
+    private class Wrapper<T> {
+        T value;
     }
 }
