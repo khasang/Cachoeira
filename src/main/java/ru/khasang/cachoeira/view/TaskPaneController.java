58098bb8397221fbaf533e4b9f10035c354df175
truesik <truesik@gmail.com>
2015-12-29 12:53:04 +0300

    Изменил код в методе isInteger на более быстрый

--- src/main/java/ru/khasang/cachoeira/view/TaskPaneController.java
+++ src/main/java/ru/khasang/cachoeira/view/TaskPaneController.java
@@ -341,10 +341,25 @@ public class TaskPaneController {
             }
 
             private boolean isInteger(String string) {
-                try {
-                    Integer.parseInt(string);
-                } catch (Exception e) {
+                if (string == null) {
                     return false;
+                }
+                int length = string.length();
+                if (length == 0) {
+                    return false;
+                }
+                int i = 0;
+                if (string.charAt(0) == '-') {
+                    if (length == 1) {
+                        return false;
+                    }
+                    i = 1;
+                }
+                for (; i < length; i++) {
+                    char c = string.charAt(i);
+                    if (c < '0' || c > '9') {
+                        return false;
+                    }
                 }
                 return true;
             }
