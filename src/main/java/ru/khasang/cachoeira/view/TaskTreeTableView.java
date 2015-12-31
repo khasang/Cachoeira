d597c43a27e2277d52c59bb0a41854044d94219b
truesik <truesik@gmail.com>
2015-12-31 16:42:33 +0300

    Добавил класс TaskTreeTableView

---
+++ src/main/java/ru/khasang/cachoeira/view/TaskTreeTableView.java
@@ -0,0 +1,46 @@
+package ru.khasang.cachoeira.view;
+
+import com.sun.javafx.scene.control.skin.*;
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Пришлось сделать свою таблицу, чтобы вытащить вертикальный скролл и синхронизировать его с диаграммой Ганта.
+ */
+public class TaskTreeTableView<S> extends TreeTableView<S> {
+    private static final Logger logger = LoggerFactory.getLogger(TaskTreeTableView.class.getName());
+
+    private final UIControl uiControl;
+
+    public TaskTreeTableView(TreeItem<S> root, UIControl uiControl) {
+        super(root);
+        this.uiControl = uiControl;
+    }
+
+    @Override
+    protected Skin<?> createDefaultSkin() {
+        return new TaskTreeTableViewSkin<>(this, uiControl);
+    }
+
+    private class TaskTreeTableViewSkin<T> extends TreeTableViewSkin<T> {
+        public TaskTreeTableViewSkin(TreeTableView<T> tableView, UIControl uiControl) {
+            super(tableView);
+            VirtualScrollBar verticalScrollBar = null;
+            for (Node child : flow.getChildrenUnmodifiable()) {
+                if (child instanceof VirtualScrollBar) {
+                    if (((VirtualScrollBar) child).getOrientation() == Orientation.VERTICAL) {
+                        verticalScrollBar = (VirtualScrollBar) child;
+                        logger.debug("Найден вертикальный скролл таблицы");
+                    }
+                }
+            }
+            if (verticalScrollBar == null) return;
+            // Синхронизируем вертикальный скролл таблицы и диаграммы
+            verticalScrollBar.valueProperty().bindBidirectional(uiControl.taskVerticalScrollProperty());
+            logger.debug("Вертикальный скролл таблицы привязан к {} вью контроллера", uiControl.taskVerticalScrollProperty().getName());
+        }
+    }
+}
