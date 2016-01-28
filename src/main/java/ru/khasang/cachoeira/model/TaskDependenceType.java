9453be1912b6ab5d85d0ae89b24c722d740afd93
truesik <truesik@gmail.com>
2016-01-28 21:53:14 +0300

    Дописал enum'ы

--- src/main/java/ru/khasang/cachoeira/model/TaskDependenceType.java
+++ src/main/java/ru/khasang/cachoeira/model/TaskDependenceType.java
@@ -4,4 +4,8 @@ package ru.khasang.cachoeira.model;
  * Created by truesik on 22.10.2015.
  */
 public enum TaskDependenceType {
+    STARTSTART,
+    STARTFINISH,
+    FINISHSTART,
+    FINISHFINISH
 }
