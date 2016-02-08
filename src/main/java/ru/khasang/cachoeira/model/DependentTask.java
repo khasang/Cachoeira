bf54431be756ca1b9581a88e0b8a17bdf7be8037
truesik <truesik@gmail.com>
2016-02-08 19:38:53 +0300

    Поменял обычные объекты на ObjectProperty

--- src/main/java/ru/khasang/cachoeira/model/DependentTask.java
+++ src/main/java/ru/khasang/cachoeira/model/DependentTask.java
@@ -1,25 +1,42 @@
 package ru.khasang.cachoeira.model;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
 /**
  * Created by truesik on 22.10.2015.
  */
 public class DependentTask implements IDependentTask {
-    private ITask task;
-    private TaskDependenceType dependenceType;
+    private ObjectProperty<ITask> task = new SimpleObjectProperty<>(this, "task");
+    private ObjectProperty<TaskDependencyType> dependenceType = new SimpleObjectProperty<>(this, "dependenceType");
 
+    @Override
     public ITask getTask() {
+        return task.get();
+    }
+
+    @Override
+    public ObjectProperty<ITask> taskProperty() {
         return task;
     }
 
+    @Override
     public void setTask(ITask task) {
-        this.task = task;
+        this.task.set(task);
     }
 
-    public TaskDependenceType getDependenceType() {
+    @Override
+    public TaskDependencyType getDependenceType() {
+        return dependenceType.get();
+    }
+
+    @Override
+    public ObjectProperty<TaskDependencyType> dependenceTypeProperty() {
         return dependenceType;
     }
 
-    public void setDependenceType(TaskDependenceType dependenceType) {
-        this.dependenceType = dependenceType;
+    @Override
+    public void setDependenceType(TaskDependencyType dependenceType) {
+        this.dependenceType.set(dependenceType);
     }
 }
