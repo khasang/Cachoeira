953703d9e79858e2f32a0c93e7fcb9d660850bcd
truesik <truesik@gmail.com>
2016-02-16 22:31:27 +0300

    Причесал

--- src/main/java/ru/khasang/cachoeira/view/taskpaneganttchart/TaskPaneRelationLine.java
+++ src/main/java/ru/khasang/cachoeira/view/taskpaneganttchart/TaskPaneRelationLine.java
@@ -1,43 +1,71 @@
 package ru.khasang.cachoeira.view.taskpaneganttchart;
 
 import javafx.beans.binding.DoubleBinding;
-import javafx.beans.property.DoubleProperty;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import ru.khasang.cachoeira.model.TaskDependencyType;
 
 public class TaskPaneRelationLine extends Group {
+    @SuppressWarnings("FieldCanBeLocal")
+    private final double[] ARROW_SHAPE = {0, 0, 3, -6, -3, -6};
+    private final Color LINE_COLOR = Color.valueOf("03bdf4");
+    private final Color HOVER_LINE_COLOR = Color.valueOf("#03A9F4");
+
     public TaskPaneRelationLine(TaskPaneTaskBar parentTaskBar,
-                                TaskPaneTaskBar childTaskBar) {
-
-        BoundLine firstLine = new BoundLine(
-                parentTaskBar.layoutXProperty().add(parentTaskBar.widthProperty()),
-                parentTaskBar.layoutYProperty().add(parentTaskBar.heightProperty().divide(2)),
-                childTaskBar.layoutXProperty(),
-                parentTaskBar.layoutYProperty().add(parentTaskBar.heightProperty().divide(2))
-        );
-        BoundLine secondLine = new BoundLine(
-                childTaskBar.layoutXProperty().add(0),
-                parentTaskBar.layoutYProperty().add(parentTaskBar.heightProperty().divide(2)),
-                childTaskBar.layoutXProperty(),
-                childTaskBar.layoutYProperty().add(0)
-        );
-
-        this.getChildren().addAll(firstLine, secondLine);
+                                TaskPaneTaskBar childTaskBar,
+                                TaskDependencyType dependenceType) {
+        if (dependenceType.equals(TaskDependencyType.FINISHSTART)) {
+            BoundLine firstLine = new BoundLine(
+                    parentTaskBar.layoutXProperty().add(parentTaskBar.widthProperty()),
+                    parentTaskBar.layoutYProperty().add(parentTaskBar.heightProperty().divide(2)),
+                    childTaskBar.layoutXProperty().add(5),
+                    parentTaskBar.layoutYProperty().add(parentTaskBar.heightProperty().divide(2))
+            );
+            BoundLine secondLine = new BoundLine(
+                    childTaskBar.layoutXProperty().add(5),
+                    parentTaskBar.layoutYProperty().add(parentTaskBar.heightProperty().divide(2).add(1)),
+                    childTaskBar.layoutXProperty().add(5),
+                    childTaskBar.layoutYProperty().add(6.5)
+            );
+            Arrow arrow = new Arrow(
+                    childTaskBar.layoutXProperty().add(5),
+                    childTaskBar.layoutYProperty().add(6.5),
+                    ARROW_SHAPE);
+            this.getChildren().addAll(firstLine, secondLine, arrow);
+//            this.hoverProperty().addListener(event -> {
+//                if (this.isHover()) {
+//                    firstLine.setStroke(HOVER_LINE_COLOR);
+//                    secondLine.setStroke(HOVER_LINE_COLOR);
+//                    arrow.setFill(HOVER_LINE_COLOR);
+//                } else {
+//                    firstLine.setStroke(LINE_COLOR);
+//                    secondLine.setStroke(LINE_COLOR);
+//                    arrow.setFill(LINE_COLOR);
+//                }
+//            });
+        }
     }
 
     class BoundLine extends Line {
-        BoundLine(DoubleBinding startX, DoubleBinding startY, DoubleProperty endX, DoubleBinding endY) {
+        BoundLine(DoubleBinding startX, DoubleBinding startY, DoubleBinding endX, DoubleBinding endY) {
             startXProperty().bind(startX);
             startYProperty().bind(startY);
             endXProperty().bind(endX);
             endYProperty().bind(endY);
-            setStrokeWidth(2);
-            setStroke(Color.GRAY.deriveColor(0, 1, 1, 0.5));
-//        setStrokeLineCap(StrokeLineCap.BUTT);
-//        getStrokeDashArray().setAll(10.0, 5.0);
+            setStrokeWidth(1);
+            setStroke(LINE_COLOR);
             setMouseTransparent(true);
         }
     }
 
+    class Arrow extends Polygon {
+        Arrow(DoubleBinding x, DoubleBinding y, double... points) {
+            super(points);
+            setFill(LINE_COLOR);
+            this.layoutXProperty().bind(x);
+            this.layoutYProperty().bind(y);
+        }
+    }
 }
