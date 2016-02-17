ef6641b9a71742a8716804c6ede114afd8aeb92f
truesik <truesik@gmail.com>
2016-02-17 22:40:55 +0300

    Причесал код

--- src/main/java/ru/khasang/cachoeira/view/taskpaneganttchart/TaskPaneRelationsLayer.java
+++ src/main/java/ru/khasang/cachoeira/view/taskpaneganttchart/TaskPaneRelationsLayer.java
@@ -8,11 +8,7 @@ import org.slf4j.LoggerFactory;
 import ru.khasang.cachoeira.model.IDependentTask;
 import ru.khasang.cachoeira.model.ITask;
 import ru.khasang.cachoeira.view.UIControl;
-import ru.khasang.cachoeira.view.tooltips.TaskTooltip;
 
-/**
- * Created by Вячеслав on 12.02.2016.
- */
 public class TaskPaneRelationsLayer extends Pane {
     private static final Logger LOGGER = LoggerFactory.getLogger(TaskPaneRelationsLayer.class.getName());
 
@@ -22,43 +18,64 @@ public class TaskPaneRelationsLayer extends Pane {
     public TaskPaneRelationsLayer() {
     }
 
+    /**
+     *
+     * @param uiControl
+     */
     public void refreshRelationsDiagram(UIControl uiControl) {
         this.getChildren().clear();
-        uiControl.getController().getProject().getTaskList().stream()
-                .flatMap(task -> task.getParentTasks().stream())
-                .map(dependentTask -> dependentTask.getTask())
-                .forEach();
-        uiControl.getController().getProject().getTaskList().forEach(task -> {
-            TaskPaneTaskBar taskPaneTaskBar = createTaskBar(uiControl, task);
-            this.getChildren().add(taskPaneTaskBar);
-        });
+        for (ITask task : uiControl.getController().getProject().getTaskList()) {
+            for (IDependentTask dependentTask : task.getParentTasks()) {
+                TaskPaneTaskBar parentTaskBar = uiControl.getMainWindow().getDiagramPaneController().getTaskPaneController()
+                        .getTaskGanttChart().getTaskPaneObjectsLayer()
+                        .findTaskBarByTask(dependentTask.getTask());
+                TaskPaneTaskBar childTaskBar = uiControl.getMainWindow().getDiagramPaneController().getTaskPaneController()
+                        .getTaskGanttChart().getTaskPaneObjectsLayer()
+                        .findTaskBarByTask(task);
+                TaskPaneRelationLine relationLine = new TaskPaneRelationLine(
+                        parentTaskBar,
+                        childTaskBar,
+                        dependentTask.getDependenceType()
+                );
+                this.getChildren().add(relationLine);
+            }
+        }
         LOGGER.debug("Диаграмма связей обновлена.");
     }
 
-    public void addBackgroundTaskBar(UIControl uiControl) {
-
-    }
-
-    public void addRelation(IDependentTask dependentTask, UIControl uiControl) {
-        TaskPaneRelationLine taskPaneRelationLine = new TaskPaneRelationLine();
-        this.getChildren().add(taskPaneRelationLine);
+    /**
+     *
+     * @param dependentTask
+     * @param task
+     * @param uiControl
+     */
+    public void addRelation(IDependentTask dependentTask,
+                            ITask task,
+                            UIControl uiControl) {
+        TaskPaneTaskBar parentTaskBar = uiControl.getMainWindow().getDiagramPaneController().getTaskPaneController()
+                .getTaskGanttChart().getTaskPaneObjectsLayer()
+                .findTaskBarByTask(dependentTask.getTask());
+        TaskPaneTaskBar childTaskBar = uiControl.getMainWindow().getDiagramPaneController().getTaskPaneController()
+                .getTaskGanttChart().getTaskPaneObjectsLayer()
+                .findTaskBarByTask(task);
+        TaskPaneRelationLine relationLine = new TaskPaneRelationLine(
+                parentTaskBar,
+                childTaskBar,
+                dependentTask.getDependenceType());
+        this.getChildren().add(relationLine);
     }
 
     /**
-     * Метод для создания метки.
      *
-     * @param uiControl Контроллер вью.
-     * @param task      Задача которая присваивается к метке.
-     * @return Возвращает taskBar.
+     * @param parentTask
+     * @param childTask
      */
-    private TaskPaneTaskBar createTaskBar(UIControl uiControl,
-                                          ITask task) {
-        TaskPaneTaskBar taskPaneTaskBar = new TaskPaneTaskBar();
-        taskPaneTaskBar.initTaskRectangle(uiControl, task);
-        taskPaneTaskBar.setTask(task);
-        taskPaneTaskBar.setContextMenu(uiControl.getController(), task);
-        taskPaneTaskBar.setTooltip(new TaskTooltip(task));
-        return taskPaneTaskBar;
+    public void removeRelation(ITask parentTask,
+                               ITask childTask) {
+        this.getChildren().removeIf(node -> {
+            TaskPaneRelationLine relationLine = (TaskPaneRelationLine) node;
+            return relationLine.getParentTask().equals(parentTask) && relationLine.getChildTask().equals(childTask);
+        });
     }
 
     /**
