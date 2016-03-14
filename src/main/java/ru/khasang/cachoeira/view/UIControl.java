d68f5fdfbf8633bec800254378e0d0de0f0fcfbc
truesik <truesik@gmail.com>
2016-03-14 19:30:20 +0300

    Поправил код для избежания NullPointerException

--- src/main/java/ru/khasang/cachoeira/view/UIControl.java
+++ src/main/java/ru/khasang/cachoeira/view/UIControl.java
@@ -3,48 +3,56 @@ package ru.khasang.cachoeira.view;
 import javafx.beans.property.*;
 import ru.khasang.cachoeira.controller.Controller;
 import ru.khasang.cachoeira.controller.IController;
-import ru.khasang.cachoeira.data.ISettingsDAO;
-import ru.khasang.cachoeira.data.SettingsDAO;
+import ru.khasang.cachoeira.data.ISettingsManager;
+import ru.khasang.cachoeira.data.SettingsManager;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class UIControl {
-    public static ResourceBundle bundle;
-
     private MainWindow mainWindow;
+
     private StartWindow startWindow;
     private NewProjectWindow newProjectWindow;
-    private ISettingsDAO settingsDAO = SettingsDAO.getInstance();
+    private ISettingsManager settingsManager = SettingsManager.getInstance();
     private IController controller = new Controller();
 
-    private IntegerProperty zoomMultiplier;
-    private DoubleProperty horizontalScrollValue;
-    private DoubleProperty taskVerticalScrollValue;
-    private DoubleProperty resourceVerticalScrollValue;
-    private DoubleProperty taskHorizontalScrollValue;
-    private DoubleProperty resourceHorizontalScrollValue;
-    private DoubleProperty splitPaneDividerValue;
-    private DoubleProperty widthOfWindow;
-    private DoubleProperty heightOfWindow;
-    private BooleanProperty isMaximized;
+    // Присваиваем значения по умолчанию для тех случаев, если добавилось еще одно поле которое нужно считать из
+    // файла с настройками, но файл уже создан (чтобы не было NullPointerException).
+    public static ResourceBundle bundle = ResourceBundle.getBundle("locale.messages", new Locale("EN"));
+
+    private IntegerProperty zoomMultiplier = new SimpleIntegerProperty(this, "zoomMultiplier", 70);
+    private DoubleProperty horizontalScrollValue = new SimpleDoubleProperty(this, "horizontalScrollValue", 0);
+    private DoubleProperty taskVerticalScrollValue = new SimpleDoubleProperty(this, "taskVerticalScrollValue", 0);
+    private DoubleProperty resourceVerticalScrollValue = new SimpleDoubleProperty(this, "resourceVerticalScrollValue", 0);
+    private DoubleProperty taskHorizontalScrollValue = new SimpleDoubleProperty(this, "taskHorizontalScrollValue", 0);
+    private DoubleProperty resourceHorizontalScrollValue = new SimpleDoubleProperty(this, "resourceHorizontalScrollValue", 0);
+    private DoubleProperty splitPaneDividerValue = new SimpleDoubleProperty(this, "splitPaneDividerValue", 0.3);
+    private DoubleProperty widthOfWindow = new SimpleDoubleProperty(this, "widthOfWindow", 1280);
+    private DoubleProperty heightOfWindow = new SimpleDoubleProperty(this, "heightOfWindow", 720);
+    private BooleanProperty isMaximized = new SimpleBooleanProperty(this, "isMaximized", false);
 
     public UIControl() {
-        bundle = ResourceBundle.getBundle("locale.messages", new Locale(settingsDAO.getProgramPropertyByKey("Language")));
-        zoomMultiplier = new SimpleIntegerProperty(this, "zoomMultiplier", Double.valueOf(settingsDAO.getUIValueByKey("ZoomValue")).intValue());
-        horizontalScrollValue = new SimpleDoubleProperty(this, "horizontalScrollValue", 0);
-        taskVerticalScrollValue = new SimpleDoubleProperty(this, "taskVerticalScrollValue", 0);
-        resourceVerticalScrollValue = new SimpleDoubleProperty(this, "resourceVerticalScrollValue", 0);
-        taskHorizontalScrollValue = new SimpleDoubleProperty(this, "taskHorizontalScrollValue", 0);
-        resourceHorizontalScrollValue = new SimpleDoubleProperty(this, "resourceHorizontalScrollValue", 0);
-        splitPaneDividerValue = new SimpleDoubleProperty(this, "splitPaneDividerValue", Double.parseDouble(settingsDAO.getUIValueByKey("DiagramDividerValue")));
-        widthOfWindow = new SimpleDoubleProperty(this, "widthOfWindow", Double.parseDouble(settingsDAO.getUIValueByKey("WidthOfWindow")));
-        heightOfWindow = new SimpleDoubleProperty(this, "heightOfWindow", Double.parseDouble(settingsDAO.getUIValueByKey("HeightOfWindow")));
+        if (settingsManager.getProgramPropertyByKey("Language") != null) {
+            bundle = ResourceBundle.getBundle("locale.messages", new Locale(settingsManager.getProgramPropertyByKey("Language")));
+        }
+        if (settingsManager.getUIValueByKey("ZoomValue") != null) {
+            zoomMultiplier = new SimpleIntegerProperty(this, "zoomMultiplier", Double.valueOf(settingsManager.getUIValueByKey("ZoomValue")).intValue());
+        }
+        if (settingsManager.getUIValueByKey("DiagramDividerValue") != null) {
+            splitPaneDividerValue = new SimpleDoubleProperty(this, "splitPaneDividerValue", Double.parseDouble(settingsManager.getUIValueByKey("DiagramDividerValue")));
+        }
+        if (settingsManager.getUIValueByKey("WidthOfWindow") != null) {
+            widthOfWindow = new SimpleDoubleProperty(this, "widthOfWindow", Double.parseDouble(settingsManager.getUIValueByKey("WidthOfWindow")));
+        }
+        if (settingsManager.getUIValueByKey("HeightOfWindow") != null) {
+            heightOfWindow = new SimpleDoubleProperty(this, "heightOfWindow", Double.parseDouble(settingsManager.getUIValueByKey("HeightOfWindow")));
+        }
         isMaximized = new SimpleBooleanProperty(this, "isMaximized", isMaximized());
     }
 
     private boolean isMaximized() {
-        return settingsDAO.getUIValueByKey("MaximizedWindow").equals("1");
+        return settingsManager.getUIValueByKey("MaximizedWindow") != null && settingsManager.getUIValueByKey("MaximizedWindow").equals("1");
     }
 
     public void launchStartWindow() {
