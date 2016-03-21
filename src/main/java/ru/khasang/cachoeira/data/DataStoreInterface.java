0b19328f14aa3331446515efe5ac3fa280df9ba4
truesik <truesik@gmail.com>
2016-03-21 21:49:45 +0300

    Интерфейс для доступа к файлам проектов.

---
+++ src/main/java/ru/khasang/cachoeira/data/DataStoreInterface.java
@@ -0,0 +1,13 @@
+package ru.khasang.cachoeira.data;
+
+import ru.khasang.cachoeira.model.IResource;
+import ru.khasang.cachoeira.model.ITask;
+
+import java.util.List;
+
+public interface DataStoreInterface {
+    void createProjectFile();
+    void saveProjectToFile();
+    List<ITask> getTaskListFromFile();
+    List<IResource> getResourceListFromFile();
+}
