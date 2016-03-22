a467cc1707a7093f7f10e60f26c63bb7825e8cae
truesik <truesik@gmail.com>
2016-03-23 00:48:15 +0300

    Заготовка метода.

--- src/main/java/ru/khasang/cachoeira/data/DBSchemeManager.java
+++ src/main/java/ru/khasang/cachoeira/data/DBSchemeManager.java
@@ -4,14 +4,27 @@ import ru.khasang.cachoeira.model.IResource;
 import ru.khasang.cachoeira.model.ITask;
 
 import java.io.File;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class DBSchemeManager implements DataStoreInterface {
     private DBHelper dbHelper = DBHelper.getInstance();
 
     @Override
-    public void createProjectFile() {
-
+    public void createProjectFile(String path) {
+        try {
+            Statement statement = dbHelper.getConnection(path).createStatement();
+            statement.executeUpdate("Some shit");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                dbHelper.getConnection(path).close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
