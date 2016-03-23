dce7d4fe2579e085d9d87f461503d0a56d36c116
truesik <truesik@gmail.com>
2016-03-23 22:49:34 +0300

    Написан метод создания проекта и метод выемки из файла информации о проекте.

--- src/main/java/ru/khasang/cachoeira/data/DBSchemeManager.java
+++ src/main/java/ru/khasang/cachoeira/data/DBSchemeManager.java
@@ -1,25 +1,52 @@
 package ru.khasang.cachoeira.data;
 
+import ru.khasang.cachoeira.model.IProject;
 import ru.khasang.cachoeira.model.IResource;
 import ru.khasang.cachoeira.model.ITask;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.List;
 
 public class DBSchemeManager implements DataStoreInterface {
     private DBHelper dbHelper = DBHelper.getInstance();
 
     @Override
-    public void createProjectFile(String path) {
+    public void createProjectFile(String path, IProject project) {
+        Statement statement = null;
+        PreparedStatement preparedStatement = null;
         try {
-            Statement statement = dbHelper.getConnection(path).createStatement();
-            statement.executeUpdate("Some shit");
+            try {
+                statement = dbHelper.getConnection(path).createStatement();
+                String sql = new String(Files.readAllBytes(Paths.get(getClass().getResource("/sql/create.sql").toURI())), "UTF-8");
+                statement.executeUpdate(sql);
+                preparedStatement = dbHelper.getConnection(path).prepareStatement("INSERT INTO Project(Name, Start_Date, Finish_Date, Description) VALUES (?, ?, ?, ?);");
+                preparedStatement.setString(1, project.getName());
+                preparedStatement.setString(2, project.getStartDate().toString());
+                preparedStatement.setString(3, project.getFinishDate().toString());
+                preparedStatement.setString(4, project.getDescription());
+                preparedStatement.executeUpdate();
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
                 dbHelper.getConnection(path).close();
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -40,5 +67,32 @@ public class DBSchemeManager implements DataStoreInterface {
     @Override
     public List<IResource> getResourceListFromFile(File file) {
         return null;
+    }
+
+    @Override
+    public IProject getProjectFromFile(File file, IProject project) {
+        Statement statement = null;
+        try {
+            statement = dbHelper.getConnection(file.getPath()).createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Project;");
+            while (resultSet.next()) {
+                project.setName(resultSet.getString("Name"));
+                project.setStartDate(LocalDate.parse(resultSet.getString("Start_Date")));
+                project.setFinishDate(LocalDate.parse(resultSet.getString("Finish_Date")));
+                project.setDescription(resultSet.getString("Description"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                dbHelper.getConnection(file.getPath()).close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return project;
     }
 }
