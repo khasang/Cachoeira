e75701c9174bbb41d7df8f584edf387967924aa8
truesik <truesik@gmail.com>
2016-03-23 22:48:31 +0300

    поправки

--- src/main/java/ru/khasang/cachoeira/data/DataStoreInterface.java
+++ src/main/java/ru/khasang/cachoeira/data/DataStoreInterface.java
@@ -1,5 +1,6 @@
 package ru.khasang.cachoeira.data;
 
+import ru.khasang.cachoeira.model.IProject;
 import ru.khasang.cachoeira.model.IResource;
 import ru.khasang.cachoeira.model.ITask;
 
@@ -7,11 +8,13 @@ import java.io.File;
 import java.util.List;
 
 public interface DataStoreInterface {
-    void createProjectFile(String path);
+    void createProjectFile(String path, IProject project);
 
     void saveProjectToFile(File file);
 
     List<ITask> getTaskListFromFile(File file);
 
     List<IResource> getResourceListFromFile(File file);
+
+    IProject getProjectFromFile(File file, IProject project);
 }
