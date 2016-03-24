db989ddc4d8fc9b83f91893e8e4226b0ec65f3ef
truesik <truesik@gmail.com>
2016-03-24 23:16:05 +0300

    Написал метод для выгрузки Ресурсов

--- src/main/java/ru/khasang/cachoeira/data/DBSchemeManager.java
+++ src/main/java/ru/khasang/cachoeira/data/DBSchemeManager.java
@@ -1,8 +1,6 @@
 package ru.khasang.cachoeira.data;
 
-import ru.khasang.cachoeira.model.IProject;
-import ru.khasang.cachoeira.model.IResource;
-import ru.khasang.cachoeira.model.ITask;
+import ru.khasang.cachoeira.model.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,6 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DBSchemeManager implements DataStoreInterface {
@@ -61,20 +60,79 @@ public class DBSchemeManager implements DataStoreInterface {
 
     @Override
     public List<ITask> getTaskListFromFile(File file) {
+        List<ITask> taskList = new ArrayList<>();
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = dbHelper.getConnection(file.getPath()).createStatement();
+            resultSet = statement.executeQuery("" +
+                    "SELECT *" +
+                    "FROM ");
+            while (resultSet.next()) {
+                ITask task = new Task();
+                task.setId(resultSet.getInt("Id"));
+                task.setName(resultSet.getString("Name"));
+                task.setStartDate(LocalDate.parse(resultSet.getString("Start_Date")));
+                task.setFinishDate(LocalDate.parse(resultSet.getString("Finish_Date")));
+                task.setDuration();
+                task.setDonePercent();
+                task.setCost();
+                task.setPriorityType();
+                task.setResourceList();
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public List<IResource> getResourceListFromFile(File file) {
-        return null;
+        List<IResource> resourceList = new ArrayList<>();
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = dbHelper.getConnection(file.getPath()).createStatement();
+            resultSet = statement.executeQuery("" +
+                    "SELECT r.Id AS Id, r.Name AS Name, rt.Type AS Type, r.Email AS Email, r.Description AS Description " +
+                    "FROM Resources AS r " +
+                    "JOIN Resource_Type AS rt ON r.Type_Id = rt.Id;"
+            );
+            while (resultSet.next()) {
+                IResource resource = new Resource();
+                resource.setId(resultSet.getInt("Id"));
+                resource.setName(resultSet.getString("Name"));
+                resource.setType(ResourceType.valueOf(resultSet.getString("Type")));
+                resource.setEmail(resultSet.getString("Email"));
+                resource.setDescription(resultSet.getString("Description"));
+                resourceList.add(resource);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+//                dbHelper.getConnection(file.getPath()).close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return resourceList;
     }
 
     @Override
     public IProject getProjectFromFile(File file, IProject project) {
         Statement statement = null;
+        ResultSet resultSet = null;
         try {
             statement = dbHelper.getConnection(file.getPath()).createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM Project;");
+            resultSet = statement.executeQuery("SELECT * FROM Project;");
             while (resultSet.next()) {
                 project.setName(resultSet.getString("Name"));
                 project.setStartDate(LocalDate.parse(resultSet.getString("Start_Date")));
@@ -85,10 +143,13 @@ public class DBSchemeManager implements DataStoreInterface {
             e.printStackTrace();
         } finally {
             try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
                 if (statement != null) {
                     statement.close();
                 }
-                dbHelper.getConnection(file.getPath()).close();
+//                dbHelper.getConnection(file.getPath()).close();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
