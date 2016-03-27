369c51687542feef7fd12d5a36e25f2ef8d73241
truesik <truesik@gmail.com>
2016-03-27 22:08:25 +0300

    Изменил структуру.

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -1,18 +1,3 @@
-CREATE TABLE IF NOT EXISTS Resource_Type (
-	Id                  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	Type                TEXT                NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Priority_Type (
-	Id                  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	Type                TEXT                NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Dependency_Type (
-	Id                  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	Type                TEXT                NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS Project (
 	Name                TEXT                NOT NULL,
 	Start_Date          TEXT                NOT NULL,
@@ -27,60 +12,36 @@ CREATE TABLE IF NOT EXISTS Tasks (
 	Finish_Date         TEXT                NOT NULL,
 	Duration            INTEGER             NOT NULL,
 	Done_Percent        REAL,
-	Priority_Type_Id    INTEGER             NOT NULL,
+	Priority_Type       TEXT                NOT NULL,
 	Cost                REAL,
 	Description         TEXT,
-	Task_Group          TEXT,
-	FOREIGN KEY (Priority_Type_Id) REFERENCES Priority_Type(Id)
+	Task_Group          TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Resources (
 	Id                  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	Name                TEXT                NOT NULL,
-	Type_Id             INTEGER             NOT NULL,
+	Resource_Type       TEXT                NOT NULL,
 	Email               TEXT,
-	Description         TEXT,
-	FOREIGN KEY (Type_Id) REFERENCES Resource_Type(Id)
+	Description         TEXT
 );
 
-CREATE TABLE IF NOT EXISTS Main_Task_List_Table (
+CREATE TABLE IF NOT EXISTS Resources_By_Task (
 	Id                  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	Task_Id             INTEGER             NOT NULL,
-	Parent_Task_Id      INTEGER,
-	Child_Task_Id       INTEGER,
-	Resource_Id         INTEGER,
-	FOREIGN KEY (Task_Id) REFERENCES Tasks(Id),
-	FOREIGN KEY (Parent_Task_Id) REFERENCES Parent_Tasks(Id),
-	FOREIGN KEY (Child_Task_Id) REFERENCES Child_Tasks(Id),
-	FOREIGN KEY (Resource_Id) REFERENCES Resources(Id)
+	Resource_Id         INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS Child_Tasks (
 	Id                  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	Task_Id             INTEGER             NOT NULL,
-	Dependency_Type_Id  INTEGER,
-	FOREIGN KEY (Task_Id) REFERENCES Tasks(Id),
-	FOREIGN KEY (Dependency_Type_Id) REFERENCES Dependency_Type(Id)
+	Child_Task_Id       INTEGER             NOT NULL,
+	Dependency_Type     TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Parent_Tasks (
 	Id                  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	Task_Id             INTEGER             NOT NULL,
-	Dependency_Type_Id  INTEGER             NOT NULL,
-	FOREIGN KEY (Task_Id) REFERENCES Tasks(Id),
-    FOREIGN KEY (Dependency_Type_Id) REFERENCES Dependency_Type(Id)
+	Parent_Task_Id      INTEGER             NOT NULL,
+	Dependency_Type     TEXT                NOT NULL
 );
-
-INSERT INTO Resource_Type(Type) VALUES ('STUFF');
-INSERT INTO Resource_Type(Type) VALUES ('TOOL');
-INSERT INTO Resource_Type(Type) VALUES ('MATERIAL');
-
-INSERT INTO Priority_Type(Type) VALUES ('High');
-INSERT INTO Priority_Type(Type) VALUES ('Normal');
-INSERT INTO Priority_Type(Type) VALUES ('Low');
-
-INSERT INTO Dependency_Type(Type) VALUES ('STARTSTART');
-INSERT INTO Dependency_Type(Type) VALUES ('STARTFINISH');
-INSERT INTO Dependency_Type(Type) VALUES ('FINISHSTART');
-INSERT INTO Dependency_Type(Type) VALUES ('FINISHFINISH');
-
