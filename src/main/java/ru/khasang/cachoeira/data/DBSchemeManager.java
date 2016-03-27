eda64c4cdc12416095a1995b309740a17d693b7a
truesik <truesik@gmail.com>
2016-03-27 22:11:47 +0300

    Добавил методы сохранения Родительских и Дочерних задач, и ресурсов привязанных к задачам

--- src/main/java/ru/khasang/cachoeira/data/DBSchemeManager.java
+++ src/main/java/ru/khasang/cachoeira/data/DBSchemeManager.java
@@ -84,14 +84,14 @@ public class DBSchemeManager implements DataStoreInterface {
         try {
             for (ITask task : project.getTaskList()) {
                 preparedStatement = dbHelper.getConnection(file.getPath()).prepareStatement("" +
-                        "INSERT INTO Tasks(Name, Start_Date, Finish_Date, Duration, Done_Percent, Priority_Type_Id, Cost, Description) " +
+                        "INSERT INTO Tasks(Name, Start_Date, Finish_Date, Duration, Done_Percent, Priority_Type, Cost, Description) " +
                         "VALUES (?, ?, ?, ?, ?, ?, ?, ?);");
                 preparedStatement.setString(1, task.getName());
                 preparedStatement.setString(2, task.getStartDate().toString());
                 preparedStatement.setString(3, task.getFinishDate().toString());
                 preparedStatement.setInt(4, task.getDuration());
                 preparedStatement.setDouble(5, task.getDonePercent());
-                preparedStatement.setInt(6, getTypeId(file, "Priority_Type", task.getPriorityType().toString()));
+                preparedStatement.setString(6, task.getPriorityType().toString());
                 preparedStatement.setDouble(7, task.getCost());
                 preparedStatement.setString(8, task.getDescription());
                 preparedStatement.executeUpdate();
@@ -116,10 +116,10 @@ public class DBSchemeManager implements DataStoreInterface {
         try {
             for (IResource resource : project.getResourceList()) {
                 preparedStatement = dbHelper.getConnection(file.getPath()).prepareStatement("" +
-                        "INSERT INTO Resources(Name, Type_Id, Email, Description) " +
+                        "INSERT INTO Resources(Name, Resource_Type, Email, Description) " +
                         "VALUES (?, ?, ?, ?);");
                 preparedStatement.setString(1, resource.getName());
-                preparedStatement.setInt(2, getTypeId(file, "Resource_Type", resource.getType().toString()));
+                preparedStatement.setString(2, resource.getType().toString());
                 preparedStatement.setString(3, resource.getEmail());
                 preparedStatement.setString(4, resource.getDescription());
                 preparedStatement.executeUpdate();
@@ -138,6 +138,84 @@ public class DBSchemeManager implements DataStoreInterface {
     }
 
     @Override
+    public void saveParentTasksToFile(File file, IProject project) {
+        deletePreviousDataFromTable(file, "Parent_Tasks");
+        PreparedStatement preparedStatement = null;
+        try {
+            for (ITask task : uiControl.getController().getProject().getTaskList()) {
+                for (IDependentTask parentTask : task.getParentTasks()) {
+                    preparedStatement = dbHelper.getConnection(file.getPath()).prepareStatement("" +
+                            "INSERT INTO Parent_Tasks(Task_Id, Parent_Task_Id, Dependency_Type) " +
+                            "VALUES (?, ?, ?);");
+                    preparedStatement.setInt(1, getId(file, "Tasks", task.getName()));
+                    preparedStatement.setInt(2, getId(file, "Tasks", parentTask.getTask().getName()));
+                    preparedStatement.setString(3, parentTask.getDependenceType().toString());
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void saveChildTasksToFile(File file, IProject project) {
+        deletePreviousDataFromTable(file, "Child_Tasks");
+        PreparedStatement preparedStatement = null;
+        try {
+            for (ITask task : uiControl.getController().getProject().getTaskList()) {
+                for (IDependentTask childTask : task.getChildTasks()) {
+                    preparedStatement = dbHelper.getConnection(file.getPath()).prepareStatement("" +
+                            "INSERT INTO Child_Tasks(Task_Id, Child_Task_Id) " +
+                            "VALUES (?, ?);");
+                    preparedStatement.setInt(1, getId(file, "Tasks", task.getName()));
+                    preparedStatement.setInt(2, getId(file, "Tasks", childTask.getTask().getName()));
+//                    preparedStatement.setString(3, childTask.getDependenceType().toString());
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void saveResourcesByTask(File file, IProject project) {
+        deletePreviousDataFromTable(file, "Resources_By_Task");
+        PreparedStatement preparedStatement;
+        try {
+            for (ITask task : uiControl.getController().getProject().getTaskList()) {
+                for (IResource resource : task.getResourceList()) {
+                    preparedStatement = dbHelper.getConnection(file.getPath()).prepareStatement("" +
+                            "INSERT INTO Resources_By_Task(Task_Id, Resource_Id) " +
+                            "VALUES (?, ?);");
+                    preparedStatement.setInt(1, getId(file, "Tasks", task.getName()));
+                    preparedStatement.setInt(2, getId(file, "Resources", resource.getName()));
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
     public List<ITask> getTaskListFromFile(File file) {
         List<ITask> taskList = new ArrayList<>();
         Statement statement = null;
@@ -145,9 +223,8 @@ public class DBSchemeManager implements DataStoreInterface {
         try {
             statement = dbHelper.getConnection(file.getPath()).createStatement();
             resultSet = statement.executeQuery("" +
-                    "SELECT Tasks.Id AS Id, Name, Start_Date, Finish_Date, Duration, Done_Percent, Cost, Description, p.Type AS Type " +
-                    "FROM Tasks " +
-                    "JOIN Priority_Type AS p ON Priority_Type_Id = p.Id;");
+                    "SELECT Id, Name, Start_Date, Finish_Date, Duration, Done_Percent, Cost, Description, Priority_Type " +
+                    "FROM   Tasks;");
             while (resultSet.next()) {
                 ITask task = new Task();
 //                task.setId(resultSet.getInt("Id"));
@@ -158,7 +235,7 @@ public class DBSchemeManager implements DataStoreInterface {
                 task.setDonePercent(resultSet.getInt("Done_Percent"));
                 task.setCost(resultSet.getDouble("Cost"));
                 task.setDescription(resultSet.getString("Description"));
-                task.setPriorityType(PriorityType.valueOf(resultSet.getString("Type")));
+                task.setPriorityType(PriorityType.valueOf(resultSet.getString("Priority_Type")));
                 taskList.add(task);
             }
         } catch (SQLException e) {
@@ -186,8 +263,8 @@ public class DBSchemeManager implements DataStoreInterface {
         try {
             preparedStatement = dbHelper.getConnection(file.getPath()).prepareStatement("" +
                     "SELECT Resource_Id " +
-                    "FROM Main_Task_List_Table " +
-                    "WHERE Task_Id = ?;");
+                    "FROM   Resources_By_Task " +
+                    "WHERE  Task_Id = ?;");
             preparedStatement.setInt(1, task.getId() + 1);
             resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
@@ -221,17 +298,15 @@ public class DBSchemeManager implements DataStoreInterface {
         ResultSet resultSet = null;
         try {
             preparedStatement = dbHelper.getConnection(file.getPath()).prepareStatement("" +
-                    "SELECT p.Task_Id AS Task_Id, d.Type AS Type " +
-                    "FROM Main_Task_List_Table AS m " +
-                    "JOIN Parent_Tasks AS p ON m.Parent_Task_Id = p.Id " +
-                    "JOIN Dependency_Type AS d ON p.Dependency_Type_Id = d.Id " +
-                    "WHERE m.Task_Id = ?;");
+                    "SELECT Parent_Task_Id, Dependency_Type AS Type " +
+                    "FROM   Parent_Tasks " +
+                    "WHERE  Task_Id = ?;");
             preparedStatement.setInt(1, task.getId() + 1);
             resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 IDependentTask parentTask = new DependentTask();
                 for (ITask t : uiControl.getController().getProject().getTaskList()) {
-                    if ((t.getId() + 1) == resultSet.getInt("Task_Id")) {
+                    if ((t.getId() + 1) == resultSet.getInt("Parent_Task_Id")) {
                         parentTask.setTask(t);
                     }
                 }
@@ -262,21 +337,19 @@ public class DBSchemeManager implements DataStoreInterface {
         ResultSet resultSet = null;
         try {
             preparedStatement = dbHelper.getConnection(file.getPath()).prepareStatement("" +
-                    "SELECT c.Task_Id AS Task_Id, d.Type AS Type " +
-                    "FROM Main_Task_List_Table AS m " +
-                    "JOIN Child_Tasks AS c ON m.Child_Task_Id = c.Id " +
-                    "JOIN Dependency_Type AS d ON c.Dependency_Type_Id = d.Id " +
-                    "WHERE m.Task_Id = ?;");
+                    "SELECT Child_Task_Id, Dependency_Type AS Type " +
+                    "FROM   Child_Tasks " +
+                    "WHERE  Task_Id = ?;");
             preparedStatement.setInt(1, task.getId() + 1);
             resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 IDependentTask childTask = new DependentTask();
                 for (ITask t : uiControl.getController().getProject().getTaskList()) {
-                    if ((t.getId() + 1) == resultSet.getInt("Task_Id")) {
+                    if ((t.getId() + 1) == resultSet.getInt("Child_Task_Id")) {
                         childTask.setTask(t);
                     }
                 }
-                childTask.setDependenceType(TaskDependencyType.valueOf(resultSet.getString("Type")));
+//                childTask.setDependenceType(TaskDependencyType.valueOf(resultSet.getString("Type")));
                 childTaskList.add(childTask);
             }
         } catch (SQLException e) {
@@ -304,15 +377,14 @@ public class DBSchemeManager implements DataStoreInterface {
         try {
             statement = dbHelper.getConnection(file.getPath()).createStatement();
             resultSet = statement.executeQuery("" +
-                    "SELECT r.Id AS Id, r.Name AS Name, rt.Type AS Type, r.Email AS Email, r.Description AS Description " +
-                    "FROM Resources AS r " +
-                    "JOIN Resource_Type AS rt ON r.Type_Id = rt.Id;"
+                    "SELECT Id, Name, Resource_Type, Email, Description " +
+                    "FROM   Resources;"
             );
             while (resultSet.next()) {
                 IResource resource = new Resource();
                 resource.setId(resultSet.getInt("Id"));
                 resource.setName(resultSet.getString("Name"));
-                resource.setType(ResourceType.valueOf(resultSet.getString("Type")));
+                resource.setType(ResourceType.valueOf(resultSet.getString("Resource_Type")));
                 resource.setEmail(resultSet.getString("Email"));
                 resource.setDescription(resultSet.getString("Description"));
                 resourceList.add(resource);
@@ -384,27 +456,19 @@ public class DBSchemeManager implements DataStoreInterface {
         }
     }
 
-    /**
-     * Метод возвращающий Id типа.
-     *
-     * @param file  Файл проекта.
-     * @param table Таблица из которой нужно получить айди типа: "Priority_Type", "Resource_Type", "Dependency_Type"
-     * @param type  Тип айди которого нужно получить (в строковом виде).
-     * @return Возвращает номер айди в таблице.
-     */
-    private int getTypeId(File file, String table, String type) {
-        int resourceTypeId = 0;
+    private int getId(File file, String table, String name) {
+        int id = 0;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
         try {
             preparedStatement = dbHelper.getConnection(file.getPath()).prepareStatement("" +
                     "SELECT Id " +
                     "FROM " + table + " " +
-                    "WHERE Type = ?;");
-            preparedStatement.setString(1, type);
+                    "WHERE  Name = ?;");
+            preparedStatement.setString(1, name);
             resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
-                resourceTypeId = resultSet.getInt("Id");
+                id = resultSet.getInt("Id");
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -420,6 +484,6 @@ public class DBSchemeManager implements DataStoreInterface {
                 e.printStackTrace();
             }
         }
-        return resourceTypeId;
+        return id;
     }
 }
