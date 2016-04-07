7fe23c4642249c87c1653c2e4c29cf7cfc5ea61b
truesik <truesik@gmail.com>
2016-04-07 20:05:47 +0300

    Реализации абстрактного класса с сеткой на заднем фоне

---
+++ src/main/java/ru/khasang/cachoeira/view/ganttplan/gridlayer/TaskGanttPlanGridLayer.java
@@ -0,0 +1,9 @@
+package ru.khasang.cachoeira.view.ganttplan.gridlayer;
+
+import ru.khasang.cachoeira.view.UIControl;
+
+public class TaskGanttPlanGridLayer extends GridLayer {
+    public TaskGanttPlanGridLayer(UIControl uiControl) {
+        super(uiControl);
+    }
+}
