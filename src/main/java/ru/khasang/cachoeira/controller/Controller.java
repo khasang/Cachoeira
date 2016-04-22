fa7df15a556c1952bf462770ef9650899ba38fad
truesik <truesik@gmail.com>
2016-04-22 19:44:45 +0300

    Удалил ненужные методы

--- src/main/java/ru/khasang/cachoeira/controller/Controller.java
+++ src/main/java/ru/khasang/cachoeira/controller/Controller.java
@@ -2,12 +2,10 @@ package ru.khasang.cachoeira.controller;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.ObservableList;
 import javafx.scene.input.DataFormat;
 import ru.khasang.cachoeira.model.*;
 
 import java.time.LocalDate;
-import java.util.List;
 
 /**
  * Класс-контроллер между представлением и моделью.
@@ -28,54 +26,6 @@ public class Controller implements IController {
         return project;
     }
 
-    @Override
-    public void handleChangeProject(IProject project) {
-
-    }
-
-    @Override
-    public void handleAddTask(ITask task) {
-        project.getTaskList().add(task);
-    }
-
-    @Override
-    public void handleRemoveTask(ITask task) {
-        // Вычищяем все задачи из списка предшествующих задач
-        task.getParentTasks().clear();
-        // Вычищаем все задачи из списка последующих задач
-        task.getChildTasks().clear();
-        // Вычищяем все ресурсы из списка привязанных ресурсов
-        task.getResourceList().clear();
-        // Удаляем все связи с этой задачей
-        project.getTaskList().stream()
-                .forEach(taskFromList -> taskFromList.getParentTasks()
-                        .removeIf(dependentTask -> dependentTask.getTask().equals(task)));
-        project.getTaskList().stream()
-                .forEach(taskFromList -> taskFromList.getChildTasks()
-                        .removeIf(dependentTask -> dependentTask.getTask().equals(task)));
-        // Удаляем эту задачу
-        project.getTaskList().remove(task);
-    }
-
-    @Override
-    public void handleChangeTask(ITask task,
-                                 String taskNameField,
-                                 LocalDate taskStartDate,
-                                 LocalDate taskFinishDate,
-                                 int duration,
-                                 Double taskCost,
-                                 double taskDonePercent,
-                                 PriorityType priority,
-                                 ObservableList<IResource> resources) {
-        task.setName(taskNameField);
-        task.setStartDate(taskStartDate);
-        task.setFinishDate(taskFinishDate);
-        task.setCost(taskCost);
-        task.setPriorityType(priority);
-        task.setDonePercent((int) taskDonePercent);
-        task.setResourceList(resources);
-    }
-
     public ITask getSelectedTask() {
         return selectedTask.get();
     }
@@ -86,44 +36,6 @@ public class Controller implements IController {
 
     public ObjectProperty<ITask> selectedTaskProperty() {
         return selectedTask;
-    }
-
-    @Override
-    public void handleAddResource(IResource resource) {
-        project.getResourceList().add(resource);
-    }
-
-    @Override
-    public void handleRemoveResource(IResource resource) {
-        // Удаляем этот ресурс из всех привязанных к нему задач
-        getProject().getTaskList()
-                .stream()
-                .filter(task -> task.getResourceList().contains(resource))
-                .forEach(task -> task.getResourceList().remove(resource));
-        // Удаляем этот ресурс из проекта
-        project.getResourceList().remove(resource);
-    }
-
-    @Override
-    public void handleChangeResource(IResource resource,
-                                     String resourceName,
-                                     String email,
-                                     ResourceType type,
-                                     List<ITask> tasks) {
-        resource.setName(resourceName);
-        resource.setEmail(email);
-        resource.setType(type);
-
-        for (ITask task : project.getTaskList()) {
-            for (ITask tas : tasks) {
-                if (!task.equals(tas) && task.getResourceList().contains(resource)) {
-                    task.removeResource(resource);
-                }
-                if (!tas.getResourceList().contains(resource)) {
-                    tas.addResource(resource);
-                }
-            }
-        }
     }
 
     @Override
@@ -149,8 +61,8 @@ public class Controller implements IController {
             project = new Project();
         }
         project.setName(projectName);
-        project.setStartDate(startDate);
-        project.setFinishDate(finishDate);
+        project.setStartDateAndVerify(startDate);
+        project.setFinishDateAndVerify(finishDate);
         project.setDescription(description);
         setProject(project);
     }
--- src/main/java/ru/khasang/cachoeira/controller/IController.java
+++ src/main/java/ru/khasang/cachoeira/controller/IController.java
@@ -7,45 +7,16 @@ import ru.khasang.cachoeira.model.*;
 import java.time.LocalDate;
 import java.util.List;
 
-/**
- * Created by truesik on 26.10.2015.
- */
 public interface IController {
     IProject getDefaultProject();
 
     IProject getProject();
-
-    void handleChangeProject(IProject project);
-
-    void handleAddTask(ITask task);
-
-    void handleRemoveTask(ITask task);
-
-    void handleChangeTask(ITask task,
-                          String taskNameField,
-                          LocalDate taskStartDate,
-                          LocalDate taskFinishDate,
-                          int duration,
-                          Double taskCost,
-                          double taskDonePercent,
-                          PriorityType taskPriorityType,
-                          ObservableList<IResource> resources);
 
     ITask getSelectedTask();
 
     void setSelectedTask(ITask task);
 
     ObjectProperty<ITask> selectedTaskProperty();
-
-    void handleAddResource(IResource resource);
-
-    void handleRemoveResource(IResource resource);
-
-    void handleChangeResource(IResource resource,
-                              String resourceName,
-                              String email,
-                              ResourceType type,
-                              List<ITask> tasks);
 
     IResource getSelectedResource();
 
