e5a2169d98e7350748ff263a8ce72ebeb025360e
truesik <truesik@gmail.com>
2016-05-05 00:16:19 +0300

    Настройки задачи

---
+++ src/main/java/ru/khasang/cachoeira/view/mainwindow/properties/TaskPropertiesPane.java
@@ -0,0 +1,12 @@
+package ru.khasang.cachoeira.view.mainwindow.properties;
+
+import javafx.scene.layout.VBox;
+
+public class TaskPropertiesPane extends VBox {
+    private static final double FIRST_COLUMN_PERCENT_WIDTH = 30;
+    private static final double SECOND_COLUMN_PERCENT_WIDTH = 70;
+
+    public void createPane() {
+
+    }
+}
