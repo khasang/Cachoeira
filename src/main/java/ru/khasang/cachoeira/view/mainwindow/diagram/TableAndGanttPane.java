a1ecdea7409039cf92f8e8b45ddafdfc86588a6e
truesik <truesik@gmail.com>
2016-05-06 21:26:11 +0300

    Добавил контроллер

--- src/main/java/ru/khasang/cachoeira/view/mainwindow/diagram/TableAndGanttPane.java
+++ src/main/java/ru/khasang/cachoeira/view/mainwindow/diagram/TableAndGanttPane.java
@@ -7,6 +7,7 @@ import javafx.scene.control.SplitPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import ru.khasang.cachoeira.vcontroller.MainWindowController;
 import ru.khasang.cachoeira.view.mainwindow.diagram.buttonbox.AbstractButtonsBox;
 import ru.khasang.cachoeira.view.mainwindow.diagram.ganttplan.AbstractGanttPlan;
 import ru.khasang.cachoeira.view.mainwindow.diagram.tables.AbstractTableView;
@@ -14,6 +15,7 @@ import ru.khasang.cachoeira.view.mainwindow.diagram.tables.AbstractTableView;
 public abstract class TableAndGanttPane extends VBox {
     private static final double ROW_HEIGHT = 31;
 
+    protected MainWindowController controller;
     protected AbstractTableView tableView;
     protected AbstractGanttPlan ganttPlan;
     protected AbstractButtonsBox buttonBox;
@@ -23,7 +25,7 @@ public abstract class TableAndGanttPane extends VBox {
         splitPane.setDividerPosition(0, 0.3);
         VBox.setVgrow(splitPane, Priority.ALWAYS);
         // Связываем разделитель таблицы и диаграммы на вкладке Задачи с разделителем на вкладке Ресурсы
-//        splitPane.getDividers().get(0).positionProperty().bindBidirectional(uiControl.splitPaneDividerValueProperty());
+        splitPane.getDividers().get(0).positionProperty().bindBidirectional(controller.splitPaneDividerValueProperty());
         HBox hBox = createButtonsBox();
         VBox.setVgrow(hBox, Priority.NEVER);
         this.getChildren().addAll(splitPane, hBox);
@@ -37,11 +39,13 @@ public abstract class TableAndGanttPane extends VBox {
     private Node createTableView() {
         VBox.setVgrow(tableView, Priority.ALWAYS);
         tableView.setFixedCellSize(ROW_HEIGHT);
+        tableView.createTable();
+        tableView.bindScrollsToController();
 
         ScrollBar horizontalScrollBar = new ScrollBar();
         horizontalScrollBar.setOrientation(Orientation.HORIZONTAL);
         horizontalScrollBar.visibleAmountProperty().bind(tableView.widthProperty());
-//        horizontalScrollBar.valueProperty().bindBidirectional(uiControl.taskHorizontalScrollValueProperty());
+        horizontalScrollBar.valueProperty().bindBidirectional(controller.taskHorizontalScrollValueProperty());
         return new VBox(tableView, horizontalScrollBar);
     }
 
--- src/main/java/ru/khasang/cachoeira/view/mainwindow/diagram/TaskPane.java
+++ src/main/java/ru/khasang/cachoeira/view/mainwindow/diagram/TaskPane.java
@@ -1,11 +1,13 @@
 package ru.khasang.cachoeira.view.mainwindow.diagram;
 
+import ru.khasang.cachoeira.vcontroller.MainWindowController;
 import ru.khasang.cachoeira.view.mainwindow.diagram.buttonbox.AbstractButtonsBox;
 import ru.khasang.cachoeira.view.mainwindow.diagram.ganttplan.AbstractGanttPlan;
 import ru.khasang.cachoeira.view.mainwindow.diagram.tables.AbstractTableView;
 
 public class TaskPane extends TableAndGanttPane {
-    public TaskPane(AbstractTableView tableView, AbstractGanttPlan ganttPlan, AbstractButtonsBox buttonsBox) {
+    public TaskPane(MainWindowController controller, AbstractTableView tableView, AbstractGanttPlan ganttPlan, AbstractButtonsBox buttonsBox) {
+        this.controller = controller;
         this.tableView = tableView;
         this.ganttPlan = ganttPlan;
         this.buttonBox = buttonsBox;
