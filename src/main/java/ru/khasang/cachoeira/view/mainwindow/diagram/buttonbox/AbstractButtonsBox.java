20a0a711b60caf2e2d325674dcc990cc97f49bbb
truesik <truesik@gmail.com>
2016-05-06 18:29:39 +0300

    Абстрактный класс с кнопками в нижней части главного окна

---
+++ src/main/java/ru/khasang/cachoeira/view/mainwindow/diagram/buttonbox/AbstractButtonsBox.java
@@ -0,0 +1,38 @@
+package ru.khasang.cachoeira.view.mainwindow.diagram.buttonbox;
+
+import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import ru.khasang.cachoeira.view.MaterialButton;
+
+public abstract class AbstractButtonsBox extends HBox{
+    protected MaterialButton addButton;
+    protected MaterialButton removeButton;
+    protected Slider zoomSlider;
+
+    public AbstractButtonsBox() {
+        addButton = new MaterialButton("", new ImageView(getClass().getResource("/img/ic_add.png").toExternalForm()));
+        removeButton = new MaterialButton("", new ImageView(getClass().getResource("/img/ic_remove.png").toExternalForm()));
+    }
+
+    public void createButtonsBox() {
+        Region separateRegion = new Region();
+        HBox.setHgrow(separateRegion, Priority.ALWAYS);
+        zoomSlider = new Slider();
+        this.getChildren().addAll(addButton, removeButton, separateRegion, zoomSlider);
+    }
+
+    public MaterialButton getAddButton() {
+        return addButton;
+    }
+
+    public MaterialButton getRemoveButton() {
+        return removeButton;
+    }
+
+    public Slider getZoomSlider() {
+        return zoomSlider;
+    }
+}
