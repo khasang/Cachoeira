40ede8bcfb2f31b4643463b32baf339256509b72
truesik <truesik@gmail.com>
2016-05-07 13:54:17 +0300

    Интерфейс для кнопок стартового окна

---
+++ src/main/java/ru/khasang/cachoeira/view/startwindow/IButtonsBox.java
@@ -0,0 +1,11 @@
+package ru.khasang.cachoeira.view.startwindow;
+
+import ru.khasang.cachoeira.view.MaterialButton;
+
+public interface IButtonsBox {
+    void createButtonsBox();
+
+    MaterialButton getCreateProjectButton();
+
+    MaterialButton getOpenProjectButton();
+}
