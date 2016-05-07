adf57e5a27aae2dcdba03ffcd8ed2d1f53e9001d
truesik <truesik@gmail.com>
2016-05-07 15:24:43 +0300

    Интерфейс кнопок и его реализация

---
+++ src/main/java/ru/khasang/cachoeira/view/createnewprojectwindow/IButtonsBox.java
@@ -0,0 +1,11 @@
+package ru.khasang.cachoeira.view.createnewprojectwindow;
+
+import ru.khasang.cachoeira.view.MaterialButton;
+
+public interface IButtonsBox {
+    void createButtonsBox();
+
+    MaterialButton getCreateNewProjectButton();
+
+    MaterialButton getCancelButton();
+}
