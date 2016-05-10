56d4ede7e93c3357e4f80ac73156e70088e67448
truesik <truesik@gmail.com>
2016-05-10 18:28:07 +0300

    Изменил обычную таблицу на древовидную

--- src/main/java/ru/khasang/cachoeira/commands/project/DragNDropResourceCommand.java
+++ src/main/java/ru/khasang/cachoeira/commands/project/DragNDropResourceCommand.java
@@ -1,17 +1,17 @@
 package ru.khasang.cachoeira.commands.project;
 
-import javafx.scene.control.TableRow;
+import javafx.scene.control.TreeTableRow;
 import ru.khasang.cachoeira.commands.Command;
 import ru.khasang.cachoeira.model.IProject;
 import ru.khasang.cachoeira.model.IResource;
 
 public class DragNDropResourceCommand implements Command {
     private final IProject project;
-    private final TableRow<IResource> row;
+    private final TreeTableRow<IResource> row;
     private final int draggedIndex;
     private IResource draggedResource;
 
-    public DragNDropResourceCommand(IProject project, TableRow<IResource> row, int draggedIndex) {
+    public DragNDropResourceCommand(IProject project, TreeTableRow<IResource> row, int draggedIndex) {
         this.project = project;
         this.row = row;
         this.draggedIndex = draggedIndex;
@@ -22,7 +22,7 @@ public class DragNDropResourceCommand implements Command {
         draggedResource = project.getResourceList().remove(draggedIndex);
         int dropIndex;
         if (row.isEmpty()) {
-            dropIndex = row.getTableView().getItems().size();
+            dropIndex = row.getTreeTableView().getRoot().getChildren().size();
         } else {
             dropIndex = row.getIndex();
         }
