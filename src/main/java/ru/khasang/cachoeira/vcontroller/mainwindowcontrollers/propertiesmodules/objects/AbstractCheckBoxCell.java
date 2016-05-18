4481056e3c066d79381994f58c23114cc7f6317e
truesik <truesik@gmail.com>
2016-05-18 17:04:34 +0300

    abstract checkbox cell

---
+++ src/main/java/ru/khasang/cachoeira/vcontroller/mainwindowcontrollers/propertiesmodules/objects/AbstractCheckBoxCell.java
@@ -0,0 +1,30 @@
+package ru.khasang.cachoeira.vcontroller.mainwindowcontrollers.propertiesmodules.objects;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TableCell;
+
+public abstract class AbstractCheckBoxCell<S, T> extends TableCell<S, T> {
+    protected S currentRow;
+
+    @Override
+    protected void updateItem(T item, boolean empty) {
+        super.updateItem(item, empty);
+        this.setAlignment(Pos.CENTER);
+        currentRow = (S) this.getTableRow().getItem();
+        if (empty) {
+            this.setText(null);
+            this.setGraphic(null);
+        } else {
+            CheckBox checkBox = createCheckBox();
+            // Расставляем галочки на нужных строках
+            this.setSelectedCheckBox(checkBox);
+            // Заполняем столбец чек-боксами
+            this.setGraphic(checkBox);
+        }
+    }
+
+    protected abstract CheckBox createCheckBox();
+
+    protected abstract void setSelectedCheckBox(CheckBox checkBox);
+}
