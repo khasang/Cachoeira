a7459a4c711d76c4ba3175820b45049c9838990f
truesik <truesik@gmail.com>
2016-05-19 23:05:27 +0300

    take data manipulations to another class

--- src/main/java/ru/khasang/cachoeira/vcontroller/rowfactories/RecentProjectsRowFactory.java
+++ src/main/java/ru/khasang/cachoeira/vcontroller/rowfactories/RecentProjectsRowFactory.java
@@ -1,18 +1,12 @@
 package ru.khasang.cachoeira.vcontroller.rowfactories;
 
-import javafx.collections.FXCollections;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.util.Callback;
-import ru.khasang.cachoeira.data.DBSchemeManager;
-import ru.khasang.cachoeira.data.DataStoreInterface;
-import ru.khasang.cachoeira.model.IProject;
-import ru.khasang.cachoeira.model.ITask;
-import ru.khasang.cachoeira.model.Project;
+import ru.khasang.cachoeira.data.DataService;
 import ru.khasang.cachoeira.properties.RecentProjectsController;
 import ru.khasang.cachoeira.properties.SettingsManager;
-import ru.khasang.cachoeira.vcontroller.MainWindowController;
 import ru.khasang.cachoeira.vcontroller.StartWindowController;
 
 import java.io.File;
@@ -31,26 +25,10 @@ public class RecentProjectsRowFactory implements Callback<TableView<File>, Table
             if (event.isPrimaryButtonDown() && event.getClickCount() == 2 && !row.isEmpty()) {
                 File recentProject = row.getItem();
                 if (recentProject.exists()) {
-                    DataStoreInterface storeInterface = new DBSchemeManager();
-                    IProject project = storeInterface.getProjectFromFile(recentProject, new Project());
-                    project.setResourceList(FXCollections.observableArrayList(storeInterface.getResourceListFromFile(recentProject)));
-                    project.setTaskList(FXCollections.observableArrayList(storeInterface.getTaskListFromFile(recentProject)));
-                    for (ITask task : project.getTaskList()) {
-                        task.setResourceList(FXCollections.observableArrayList(storeInterface.getResourceListByTaskFromFile(recentProject, project, task)));
-                    }
-                    for (ITask task : project.getTaskList()) {
-                        task.setParentTasks(FXCollections.observableArrayList(storeInterface.getParentTaskListByTaskFromFile(recentProject, project, task)));
-                    }
-                    for (ITask task : project.getTaskList()) {
-                        task.setChildTasks(FXCollections.observableArrayList(storeInterface.getChildTaskListByTaskFromFile(recentProject, project, task)));
-                    }
-
-                    RecentProjectsController.getInstance().addRecentProject(recentProject);
+                    DataService.getInstance().loadProject(recentProject);
                     if (controller.getView().getStage().isShowing()) {
                         controller.getView().getStage().close(); //закрываем стартовое окно
                     }
-                    MainWindowController mainWindowController = new MainWindowController(recentProject, project);
-                    mainWindowController.launch();
                 } else {
                     Alert alert = new Alert(Alert.AlertType.ERROR, "File does not exist");
                     alert.showAndWait();
