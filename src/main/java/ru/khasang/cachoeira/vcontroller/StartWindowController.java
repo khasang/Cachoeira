c42e5e8e859da3965216eae46cccdbbc11f08bf0
truesik <truesik@gmail.com>
2016-05-20 12:58:46 +0300

    simplified

--- src/main/java/ru/khasang/cachoeira/vcontroller/StartWindowController.java
+++ src/main/java/ru/khasang/cachoeira/vcontroller/StartWindowController.java
@@ -1,14 +1,9 @@
 package ru.khasang.cachoeira.vcontroller;
 
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableView;
 import javafx.stage.FileChooser;
-import ru.khasang.cachoeira.data.DBSchemeManager;
-import ru.khasang.cachoeira.data.DataStoreInterface;
-import ru.khasang.cachoeira.model.IProject;
-import ru.khasang.cachoeira.model.ITask;
-import ru.khasang.cachoeira.model.Project;
+import ru.khasang.cachoeira.data.DataService;
 import ru.khasang.cachoeira.properties.RecentProjectsController;
 import ru.khasang.cachoeira.vcontroller.rowfactories.RecentProjectsRowFactory;
 import ru.khasang.cachoeira.view.IView;
@@ -57,24 +52,8 @@ public class StartWindowController {
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CACH", "*.cach"));
         File file = fileChooser.showOpenDialog(view.getStage());
         if (file != null) {
-            DataStoreInterface storeInterface = new DBSchemeManager();
-            IProject project = storeInterface.getProjectFromFile(file, new Project());
-            project.setResourceList(FXCollections.observableArrayList(storeInterface.getResourceListFromFile(file)));
-            project.setTaskList(FXCollections.observableArrayList(storeInterface.getTaskListFromFile(file)));
-            for (ITask task : project.getTaskList()) {
-                task.setResourceList(FXCollections.observableArrayList(storeInterface.getResourceListByTaskFromFile(file, project, task)));
-            }
-            for (ITask task : project.getTaskList()) {
-                task.setParentTasks(FXCollections.observableArrayList(storeInterface.getParentTaskListByTaskFromFile(file, project, task)));
-            }
-            for (ITask task : project.getTaskList()) {
-                task.setChildTasks(FXCollections.observableArrayList(storeInterface.getChildTaskListByTaskFromFile(file, project, task)));
-            }
-
-            RecentProjectsController.getInstance().addRecentProject(file);
+            DataService.getInstance().loadProject(file);
             view.getStage().close();
-            MainWindowController mainWindowController = new MainWindowController(file, project);
-            mainWindowController.launch();
         }
     }
 
