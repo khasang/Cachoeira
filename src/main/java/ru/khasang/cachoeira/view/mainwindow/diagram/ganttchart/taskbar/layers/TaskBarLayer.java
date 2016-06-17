f048d507a452e81d0c9f5b939a014fbd94a73947
truesik <truesik@gmail.com>
2016-06-17 23:38:55 +0300

    layer interface

---
+++ src/main/java/ru/khasang/cachoeira/view/mainwindow/diagram/ganttchart/taskbar/layers/TaskBarLayer.java
@@ -0,0 +1,7 @@
+package ru.khasang.cachoeira.view.mainwindow.diagram.ganttchart.taskbar.layers;
+
+import javafx.scene.shape.Rectangle;
+
+public interface TaskBarLayer {
+    Rectangle create();
+}
