41666a89fd3d61d03af9f8f8ceffa820e0b084a5
truesik <truesik@gmail.com>
2016-06-19 19:39:27 +0300

    add mouse events that acts on dragging

--- src/main/java/ru/khasang/cachoeira/view/mainwindow/diagram/ganttchart/taskbar/layers/DomainBar.java
+++ src/main/java/ru/khasang/cachoeira/view/mainwindow/diagram/ganttchart/taskbar/layers/DomainBar.java
@@ -1,13 +1,26 @@
 package ru.khasang.cachoeira.view.mainwindow.diagram.ganttchart.taskbar.layers;
 
+import javafx.animation.Timeline;
+import javafx.scene.Cursor;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import ru.khasang.cachoeira.view.mainwindow.diagram.ganttchart.taskbar.TaskBar;
 
 public class DomainBar implements TaskBarLayer {
     private static final String BAR_COLOR = "#03A9F4";
     private static final String BORDER_BAR_COLOR = "#03bdf4";
     private static final double BAR_HEIGHT = 18;
     private static final double BAR_ARC = 5;
+    private static final double X_OFFSET = 0.5;
+    private static final double DURATION = 200;
+
+    private final TaskBar taskBar;
+    private Delta dragDelta;
+
+    public DomainBar(TaskBar taskBar) {
+        this.taskBar = taskBar;
+    }
 
     @Override
     public Rectangle create() {
@@ -17,6 +30,54 @@ public class DomainBar implements TaskBarLayer {
         rectangle.setArcHeight(BAR_ARC);
         rectangle.setArcWidth(BAR_ARC);
         rectangle.setHeight(BAR_HEIGHT);
+
+        enableDrag(rectangle);
         return rectangle;
+    }
+
+    /**
+     * Метод который включает возможность перемещения метки с помощью мышки по оси Х
+     *
+     * @param rectangle
+     */
+    private void enableDrag(Rectangle rectangle) {
+        dragDelta = new Delta();
+        rectangle.setOnMousePressed(this::mousePressedHandler);
+        rectangle.setOnMouseDragged(event -> mouseDraggedHandler(event, rectangle));
+        rectangle.setOnMouseReleased(this::mouseReleasedHandler);
+    }
+
+    private void mousePressedHandler(MouseEvent event) {
+        // Выделяем нужный элемент в таблице
+        taskBar.setTaskSelected();
+        if (event.isPrimaryButtonDown()) {
+            // record a delta distance for the drag and drop operation.
+            dragDelta.x = taskBar.getLayoutX() - event.getSceneX();
+            taskBar.getScene().setCursor(Cursor.MOVE);
+        }
+    }
+
+    private void mouseDraggedHandler(MouseEvent event, Rectangle rectangle) {
+        if (event.isPrimaryButtonDown()) {
+            double newX = event.getSceneX() + dragDelta.x;
+            if (newX > 0
+                    && newX + rectangle.getWidth() <= taskBar.getParent().getBoundsInParent().getWidth()) {
+                taskBar.setLayoutX(newX - X_OFFSET);
+            }
+        }
+    }
+
+    private void mouseReleasedHandler(MouseEvent event) {
+        taskBar.getScene().setCursor(Cursor.DEFAULT);
+        Timeline timeline = taskBar.createTimelineAnimation(
+                taskBar.layoutXProperty(),
+                taskBar.getPixelsValueFromFullDays(
+                        taskBar.getFullDaysFromValue(taskBar.getLayoutX())) - X_OFFSET, DURATION);
+        timeline.play();
+        taskBar.executeDraggingCommand();
+    }
+
+    private class Delta {
+        double x;
     }
 }
