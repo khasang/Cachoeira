b4aa301aa5a21bf81aff2ce868c432edcfc47734
truesik <truesik@gmail.com>
2016-06-27 23:31:51 +0300

    implementation

---
+++ src/main/java/ru/khasang/cachoeira/view/mainwindow/diagram/ganttchart/TaskGanttChart.java
@@ -0,0 +1,9 @@
+package ru.khasang.cachoeira.view.mainwindow.diagram.ganttchart;
+
+import ru.khasang.cachoeira.view.mainwindow.diagram.ganttchart.layers.GanttChartLayerFactory;
+
+public class TaskGanttChart extends GanttChart {
+    public TaskGanttChart(GanttChartLayerFactory layerFactory) {
+        super(layerFactory);
+    }
+}
